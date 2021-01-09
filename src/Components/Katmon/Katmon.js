71216f7a073a39f7daaac8328d842cc5ecc30a6e
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-01-09 15:16:50 +0500

    Add class named Katmon

---
+++ src/Components/Katmon/Katmon.js
@@ -0,0 +1,53 @@
+class Katmon {
+
+	id
+	date
+	sum
+	comment
+	isCompleted
+	type
+
+	database
+
+	constructor(database) {
+		this.database = database
+	}
+
+	//* set behaviours *//
+	set setData(sumVal) {
+		this.sum = sumVal
+		
+		this.id = (new Date()).getTime()
+		this.date = this._getDateTime()
+	}
+
+	set deleteData(id) {
+		this.database.splice(this.database.findIndex(data => data.id === Number(id)), 1)
+	}
+
+	set setEdit(id) {
+		let needToEdit = this.database.findIndex(data => data.id === Number(id))
+		console.log(needToEdit)	
+	}
+
+	//* set states *//
+	set setComment(commentVal) {
+		this.comment = commentVal
+	}
+
+	set setIsCompleted(isCompletedVal) {
+		this.isCompleted = isCompletedVal
+	}
+
+	set setType(typeVal) {
+		this.type = typeVal
+	}
+
+	/**Utilities**/
+	_getDateTime() {
+		const date = new Date()
+		return `${date.getDate().toString().padStart(2, 0)}-${(date.getMonth() + 1).toString().padStart(2, 0)}-${date.getFullYear()}`
+	}
+}
+
+module.exports.Katmon = Katmon
