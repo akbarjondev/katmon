49d1e8f10d6c727156384bfe570e5d780d982556
akbarjondev <tojiyev.akbarjon@gmail.com>
2021-01-09 15:16:13 +0500

    Add some style

---
+++ src/Components/index.js
@@ -0,0 +1,11 @@
+const { Katmon } = require('./Katmon/Katmon')
+const { data } = require('./Data/Data')
+
+const katmon = new Katmon(data)
+
+// console.table(katmon.database)
+// katmon.edit(1610103570286, 'another')
+// console.table(katmon.database)
+// const { id, comment, date } = katmon
+
+// console.table(katmon)
